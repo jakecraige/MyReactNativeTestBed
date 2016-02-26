f7cba936494c9c2d992b9a57746988caf4a7084b
Jake Craige <james.craige@gmail.com>
2016-02-26 15:36:34 -0800

    Use ListView to render wine list

--- components/WineList.js
+++ components/WineList.js
@@ -1,17 +1,30 @@
 import React, {
   Component,
   Text,
+  ListView,
   View
 } from 'react-native';
 import WineListItem from './WineListItem';
 
 class WineList extends Component {
+  constructor(props) {
+    super(props);
+
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1.title !== r2.title
+    });
+
+    this.state =  {
+      dataSource: ds.cloneWithRows(props.wines),
+    };
+  }
+
   render() {
-    const {wines} = this.props;
     return (
-      <View>
-        {wines.map(wine => <WineListItem wine={wine} key={wine.title} />)}
-      </View>
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={wine => <WineListItem wine={wine} key={wine.title} />}
+      />
     );
   }
 }
